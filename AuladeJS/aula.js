a5bc3b65b4880bf11836b7e9319a60d2d1186571
Giulia Castro <maju.nogueir3.castr3@gmail.com>
2022-05-11 21:17:39 -0300

    Arrumando

---
+++ AuladeJS/aula.js
@@ -0,0 +1,75 @@
+/*Como executar em JS:*/
+
+/*
+console.log('Hello world')
+console.log('Aprendendo JS') 
+*/
+
+/*Variaveis em JS:*/
+/*
+let idade =19;
+ console.log(idade);
+
+ let altura = 1.57;
+ console.log(altura);
+
+ 
+ let nomeCompleto = ('Maria Giulia');
+ console.log(nomeCompleto);
+
+*/
+
+
+/*Constantes*/
+
+// const valorIngressoAduto=20;
+// console.log(valorIngressoAduto);
+
+
+/*Tipos Primitivos*/
+
+// let nome = 'Rafael';  //string literal
+// let idade = 19.03;   //number literal
+// let estaAprovado = true;   //boolean;
+// let sobrenome ;      //Undefined
+// let corSelecionado = null ; //Redefinir um valor
+
+
+
+/*Tipagem dinâmica*/
+/*Js vc não precisa definir o valor ele descobre no tempo de execução*/
+/* 
+
+Annt: se você usar o tyoeof no console ele lê o tipo de variavél, você também pode mudar!
+
+let nome = 'Giulia'; String
+let idade= 19; number
+let estaAprovado = true; boolean
+let sobrenome ;  undefined
+let corSelecionado = null; = igual a objeto
+*/
+
+/*Objetos*/
+
+// let pessoa = {   
+// nome: 'Giulia',
+// idade : 19,
+// estaAprovado: true,
+// sobrenome : 'Castro'
+// };
+// console.log(pessoa);
+
+// let mercado = {
+
+// nome : 'Fruta',
+// quantidade : 22,
+// preço : 22.20,
+// temNoEstoque: true
+
+// };
+
+// console.log (mercado);
+
+
+/*Arrays*/
+
